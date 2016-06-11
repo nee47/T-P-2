d41707ec4e9eb751128c58ccaa53a92df4dff0c6
nee47 <joseph_vv@hotmail.com>
2016-06-10 22:52:39 -0300

    TP2 terminado, todo funcionando, a lo mejor falta mejorar un detallito

--- multi-merge.c
+++ multi-merge.c
@@ -21,6 +21,7 @@ int comparar(const void* cad1, const void* cad2){
   return 0;
 }
 
+// Libera la memoria pedida del struct nodo
 void destruye_nodo(void* nodex){
   nodo_t* nodo = nodex;
   fclose(nodo->archivo);
@@ -28,25 +29,28 @@ void destruye_nodo(void* nodex){
   free(nodo);
 }
 
-bool procesar_linea(heap_t* heap, nodo_t* nodo, size_t capacidad){
-  bool fin = false;  
-  if (getline(&nodo->linea, &capacidad, nodo->archivo) <= 0){
+// Si el archivo tiene lineas entonces procede a encolar en el heap y devuelve true
+// Si el archivo ya notiene lineas se destruye el struc nodo y devuelve true
+// Devuelve false en caso de error al encolar
+bool procesar_linea(heap_t* heap, nodo_t** nodo, size_t* capacidad){
+  bool fin = false; 
+  if (getline(&(*nodo)->linea, capacidad, (*nodo)->archivo) <= 0){
     fin = true;
-    destruye_nodo(nodo);
+    destruye_nodo(*nodo);
   }
-  if(!fin && !heap_encolar(heap, nodo)){
+  if(!fin && !heap_encolar(heap, *nodo)){
     heap_destruir(heap, destruye_nodo);
     return false;
   }
   return true;
 }
+
 // se ejecuta asi valgrind ./multi-merge archivo1.txt archivo2.txt archivo3.txt
 int main(int argc, char** argv){
   if(argc < 2){
     printf("Uso ./multi-merge <Archivo1> <Archivo2> .. <ArchivoN>\n");
     exit(1);
   } 
-  //  bool fin = false;
   size_t  capacidad = 0;
   heap_t* heap = heap_crear(comparar);
   if(!heap) exit(1);
@@ -60,17 +64,17 @@ int main(int argc, char** argv){
       printf("No se pudo abrir algun archivo\n");
       exit(1);   
     }
-    //nodo->linea = NULL;
-    if(!procesar_linea(heap, nodo, capacidad)) exit(1);
+    nodo->linea = NULL;
+    if(!procesar_linea(heap, &nodo, &capacidad)) exit(1);
   }
   
-  //fin = false;
   nodo_t* aux;
+  
   while(!heap_esta_vacio(heap)){ 
-    //fin = false;
     aux = heap_desencolar(heap);
-    if(aux) printf("%s", aux->linea);
-    if(!procesar_linea(heap, aux, capacidad)) exit(1);
+    printf("%s", aux->linea);
+    
+    if(!procesar_linea(heap, &aux, &capacidad)) exit(1);
   }
   
   heap_destruir(heap, NULL);
--- pruebas_abb_alumnos.c
+++ pruebas_abb_alumnos.c
@@ -7,6 +7,7 @@
 #include <time.h>
 #define VOL 10000
 
+// Estructura que se va a utilizar para guardar los pares clave, dato del arbol
 typedef struct pack{
   const char** claves;
   void** valores;
@@ -14,6 +15,8 @@ typedef struct pack{
   bool* guardado;
 }pack_t;
 
+// Crea la estructura pack.
+// Deulve NULL si hay algun fallo de malloc
 pack_t* pack_crear(size_t cantidad){
   pack_t* pack = malloc(sizeof(pack_t));
   if(!pack) return NULL;
@@ -32,7 +35,8 @@ pack_t* pack_crear(size_t cantidad){
   pack->indice = 0;
   return pack;
 }
-
+// Funcion para extraer los datos del arbol, usando la primitiva in order
+// Siempre devuelve true ya que se necesitan todos los elementos 
 bool obtener_par(const char* clave, void* dato, void* packi){
   pack_t* pack = packi;
   pack->claves[pack->indice] = clave;
@@ -42,20 +46,24 @@ bool obtener_par(const char* clave, void* dato, void* packi){
   return true;
 }
 
-
+// Vector que crea cadenas random a partir de numeros random
+// Utilizable en las pruebas
+// Puede haber claves repetidas
 char** crear_vector_cadenas(size_t largo){
   size_t a, r ;
   char cad[13];
   srand((unsigned int)time(NULL));
   char** vector = malloc(sizeof(char*) * largo);
+  if(!vector) return NULL;
   for( a = 0; a < largo; a++){
-    r = /*a; */rand();
+    r = /*a;*/ rand();
     snprintf(cad, sizeof(cad),"%zu", r );
     vector[a] = strdup(cad);
   }
   return vector;
 }
 
+// Libera la memoria de todas las cadenas del vector
 void liberar_cadenas(char** vec, size_t largo){
   size_t a;
   for( a = 0; a < largo; a++){
@@ -72,7 +80,10 @@ int comparar(const char* cadena1, const char* cadena2){
   if(cad1 < cad2) return -1;
   else return 0;
 }
-//23
+
+// Dado 2 vectores uno de origen y uno de destino. Se inserta 
+// recursivamente todas las posiciones medio de un vector que tiene
+// las claves obtenidas en orden del arbol de origen
 bool guardar_nuevo(abb_t* orig, abb_t* dest, pack_t* pack, int base, int tope, size_t cantidad){
   int medio = (tope+base)/2;
   if (base>tope || base == medio){
@@ -86,6 +97,7 @@ bool guardar_nuevo(abb_t* orig, abb_t* dest, pack_t* pack, int base, int tope, s
   return false;
 }
 
+// Libera la memoria pedida de la estructuara pack
 void destruir_pack(pack_t* pack){
   free(pack->valores);
   free(pack->claves);
@@ -93,6 +105,9 @@ void destruir_pack(pack_t* pack){
   free(pack);
 }
 
+// Dado 2 arboles uno de origen, con o sin elementos y uno vacio
+// Se inserta de tal manera de que el arbol de destino, quede
+// balanceado con las misma claves y datos del arbol de origen
 void abb_rebalanceado(abb_t* orig, abb_t* dest){  
   size_t cantidad = abb_cantidad(orig);
   pack_t* pack = pack_crear(cantidad);
@@ -107,10 +122,12 @@ void abb_rebalanceado(abb_t* orig, abb_t* dest){
 }  
 
 void primera_prueba(){
+  printf("PRUEBA ABB ITER CREAR DESDE\n\n");
   abb_t* abb = abb_crear(comparar, NULL);
   int vector[9] = {1,2,3,4,5,6,7,8,9};
   char* claves_esperadas[6] = {"4", "5", "6", "7", "9"};
   size_t i= 0;
+  printf("Primer test\n");
   print_test("abb creado", abb);
   print_test("abb guardar 9",abb_guardar(abb, "9", &vector[0]));
   print_test("abb guardar 17", abb_guardar(abb, "17", &vector[1]));
@@ -122,29 +139,35 @@ void primera_prueba(){
   print_test("abb guardar 5", abb_guardar(abb, "5", &vector[7]));
   print_test("abb guardar 7", abb_guardar(abb, "7", &vector[8]));
   abb_iter_t* iter = abb_iter_crear_desde(abb, "4", "9");
-  printf("primera\n");
+  print_test("ITER CREADO ABB ITER CREAR DESDE 4 - 9", iter);
   while(!abb_iter_in_al_final(iter)){
     print_test("abb ver iter actual correcta", strcmp((char*)abb_iter_in_ver_actual(iter), claves_esperadas[i])== 0);    
     i++;
     print_test("abb avanzar ", abb_iter_in_avanzar(iter));
   }
   abb_iter_in_destruir(iter);
-  
+  print_test("ITER DESTRIODO", true);
   i = 0;
-  printf("sgnda\n");
+
+
+  printf("Segundo test\n");
   char* claves_esperadas2[5] = {"7", "9", "17" , "20", "22"};
   iter = abb_iter_crear_desde(abb, "7", NULL);
+  print_test("ITER CREADO ABB ITER CREAR DESDE 7 - NULL", iter);
   while(!abb_iter_in_al_final(iter)){
     print_test("abb ver iter actual correcta", strcmp((char*)abb_iter_in_ver_actual(iter), claves_esperadas2[i])== 0);    
     i++;
     print_test("abb avanzar ", abb_iter_in_avanzar(iter));
   }
   abb_iter_in_destruir(iter);
-  
+  print_test("ITER DESTRUIDO ", true);
   i= 0;
-  printf("trceraa\n");
+  
+
+  printf("Tercer test\n");
   char* claves_esperadas3[5] = {"4", "5", "6", "7"};
   iter = abb_iter_crear_desde(abb, "4", "8");
+  print_test("ITER CREADO ABB ITER CREAR DESDE 4 - 8 ", iter);
   while(!abb_iter_in_al_final(iter)){
     print_test("abb ver iter actual correcta", strcmp((char*)abb_iter_in_ver_actual(iter), claves_esperadas3[i])== 0);    
     i++;
@@ -152,25 +175,33 @@ void primera_prueba(){
   }
   
   abb_iter_in_destruir(iter);
-  
+  print_test("ITER DESTRUIDO", true);
   i = 0;
-  printf("cuarto \n");
+
+
+  printf("Cuarto test \n");
   char* claves_esperadas4[3] = {"17", "20", "22"}; 
   iter = abb_iter_crear_desde(abb, "13", "22");
+  print_test("ITER CREADO ABB ITER CREAR DESDE 13 - 22", iter);
   while(!abb_iter_in_al_final(iter)){
     print_test("abb ver iter actual correcta", strcmp((char*)abb_iter_in_ver_actual(iter), claves_esperadas4[i])== 0);    
     i++;
     print_test("abb avanzar ", abb_iter_in_avanzar(iter));
   }
   abb_iter_in_destruir(iter);
+  print_test("ITER DESTRUIDO", true);
+  
   
   iter = abb_iter_crear_desde(abb, "9", "4");
   print_test("iter esta al final", abb_iter_in_al_final(iter));
   abb_iter_in_destruir(iter);
 
   /* probando funcionalidad de la anterior primitiva */
+  printf("PRUEBA PARA VERIFICAR LA FUNCIONALIDAD DE ABB ITER IN CREAR\n");
   char* claves_esperadas5[9] = {"3", "4", "5", "6", "7", "9", "17", "20", "22"}; 
+ 
   iter = abb_iter_in_crear(abb);
+  print_test("ITER CREADO ABB ITER IN CREAR", iter);
   i = 0;
   while(!abb_iter_in_al_final(iter)){
     print_test("abb ver iter actual correcta", strcmp((char*)abb_iter_in_ver_actual(iter), claves_esperadas5[i])== 0);    
@@ -178,66 +209,60 @@ void primera_prueba(){
     print_test("abb avanzar ", abb_iter_in_avanzar(iter));
   }
   abb_iter_in_destruir(iter);
-    
+  print_test("ITER DESTRUIDO", true);
   abb_destruir(abb);
 }
 
  
-void prueba_balanceado_izquierda(){
-  int a = 3, x=23;
+void prueba_balanceado(){
+  printf("PRUEBA ABB REBALANCEADO \n\n");
   abb_t* abb = abb_crear(comparar, NULL);
-  
-  abb_guardar(abb, "200", &a);
-  abb_guardar(abb, "190", &a);
-  abb_guardar(abb, "180", &a);
-  abb_guardar(abb, "170", &a);
-  abb_guardar(abb, "160", &a);
-  abb_guardar(abb, "150", &a);
-  abb_guardar(abb, "140", &a);
-  abb_guardar(abb, "130", &a);
-  abb_guardar(abb, "120", &a);
-  abb_guardar(abb, "110", &a);
-  abb_guardar(abb, "100", &a);
-  abb_guardar(abb, "90", &a); 
-  //
-  abb_guardar(abb, "80", &a);
-  abb_guardar(abb, "70", &a);
-  abb_guardar(abb, "60", &a);
-  abb_guardar(abb, "40", &a);
-  abb_guardar(abb, "32", &a);
-  abb_guardar(abb, "30", &a);
-  abb_guardar(abb, "29", &a);
-  abb_guardar(abb, "28", &a);
-  abb_guardar(abb, "27", &a);
-  abb_guardar(abb, "26", &a);
-  abb_guardar(abb, "25", &a);
-  abb_guardar(abb, "24", &a); 
-  abb_guardar(abb, "23", &x);
-  abb_guardar(abb, "22", &a);
-  abb_guardar(abb, "21", &a);
-  //abb_guardar(abb, "20", &a);
-  abb_guardar(abb, "19", &a);
-  abb_guardar(abb, "18", &a);
-  abb_guardar(abb, "17", &a);
-  abb_guardar(abb, "16", &a);
-  abb_guardar(abb, "15", &a);
-  abb_guardar(abb, "14", &a);
-  abb_guardar(abb, "13", &a);
-  abb_guardar(abb, "12", &a); 
-  
+  int vec[23] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+  print_test("abb guardar", abb_guardar(abb, "200", &vec[0]));
+  print_test("abb guardar", abb_guardar(abb, "190", &vec[1]));
+  print_test("abb guardar", abb_guardar(abb, "180", &vec[2]));
+  print_test("abb guardar", abb_guardar(abb, "170", &vec[3]));
+  print_test("abb guardar", abb_guardar(abb, "160", &vec[4]));
+  print_test("abb guardar", abb_guardar(abb, "150", &vec[5]));
+  print_test("abb guardar", abb_guardar(abb, "140", &vec[6]));
+  print_test("abb guardar", abb_guardar(abb, "130", &vec[7]));
+  print_test("abb guardar", abb_guardar(abb, "120", &vec[8]));
+  print_test("abb guardar", abb_guardar(abb, "110", &vec[9]));
+  print_test("abb guardar", abb_guardar(abb, "100", &vec[10]));
+  print_test("abb guardar", abb_guardar(abb, "90", &vec[11])); 
+  print_test("abb guardar", abb_guardar(abb, "80", &vec[12]));
+  print_test("abb guardar", abb_guardar(abb, "70", &vec[13]));
+  print_test("abb guardar", abb_guardar(abb, "60", &vec[14]));
+  print_test("abb guardar", abb_guardar(abb, "40", &vec[15]));
+  print_test("abb guardar", abb_guardar(abb, "32", &vec[16]));
+  print_test("abb guardar", abb_guardar(abb, "30", &vec[17]));
+  print_test("abb guardar", abb_guardar(abb, "29", &vec[18]));
+  print_test("abb guardar", abb_guardar(abb, "28", &vec[19]));
+  print_test("abb guardar", abb_guardar(abb, "27", &vec[20]));
+  print_test("abb guardar", abb_guardar(abb, "26", &vec[21]));
+  print_test("abb guardar", abb_guardar(abb, "25", &vec[22]));
+
+  char* vec_claves[23] = {"200", "190", "180", "170", "160", "150", "140", "130", "120", "110", "100", "90", "80", "70", "60", "40", "32", "30", "29", "28", "27", "26", "25"};
   abb_t* abb2 = abb_crear(comparar, NULL);
-  // 24 25 26 27 28 29 30 32 40 60 70 
-  
-  
+  print_test("abb2 creado", abb2);
+  print_test("abb2 cantidad es 0", abb_cantidad(abb2) == 0);
   abb_rebalanceado(abb, abb2);
   print_test("arbol1 no balanceado", !balanceado(abb));
-  print_test("arbol2  balanceado", balanceado(abb2));
-  print_test("obtener clave 23 es 23n", *(int*)abb_obtener(abb2, "23")== 23);
+  print_test("arbol2 despues de balancear esta  balanceado", balanceado(abb2));
+  print_test("la cantidad de abb2 es la misma que abb", abb_cantidad(abb) == abb_cantidad(abb2));
+  
+  for(size_t i = 0 ; i < 23; i++){
+    print_test(" clave pertenece abb pertenece a abb2 ", abb_pertenece(abb2, vec_claves[i]));
+    print_test("obtener dato es el asociado a la clave", *(int*)abb_obtener(abb, vec_claves[i]) == vec[i]);
+
+  }
+
   abb_destruir(abb);
   abb_destruir(abb2);
 }
 
 void prueba_volumen(){
+  printf("PRUEBAS VOLUMEN \n\n");
   bool ok;
   char** vec = crear_vector_cadenas(VOL);
   abb_t* abb = abb_crear(comparar, NULL);
@@ -248,8 +273,7 @@ void prueba_volumen(){
     ok = abb_guardar(abb, vec[a], &a);
     if(!ok) break;
   }
-  
-  print_test("elemetnos guardados", ok);
+  print_test("elementos guardados", ok);
   
   print_test("el abb esta desbalanceado", !balanceado(abb));
   
@@ -258,18 +282,20 @@ void prueba_volumen(){
   abb_rebalanceado(abb, abb2);
   
   print_test("el abb2 esta balanceado", balanceado(abb2));
+ 
+  for(a = 0; a < VOL; a++) {
+    ok = abb_pertenece(abb2, vec[a]); 
+    if (!ok) break;
+  }
   
+  print_test("Todos los elementos pertenecen", ok);
   abb_destruir(abb);
   abb_destruir(abb2);
   liberar_cadenas(vec, VOL);
 }
-// elementos   tiempo
-//  100        0.00257
-//  1000       0.164
-//  10000      17.2
 
 void pruebas_abb_alumnos(){
   primera_prueba();
-  prueba_balanceado_izquierda();   
+  prueba_balanceado();   
   prueba_volumen();
 }
